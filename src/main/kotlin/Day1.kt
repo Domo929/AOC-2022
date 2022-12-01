e8ea852b1136e80a58ff0766e174b1ffaed40e8b
Dominic Cupo <Dominic.Cupo@gmail.com>
2022-12-01 16:54:46 -0500

    Day 1

---
+++ src/main/kotlin/Day1.kt
@@ -0,0 +1,33 @@
+import java.io.File
+
+class Day1(path: String) {
+    private val lines: List<String> = File(path).readLines()
+
+    fun solve(): Pair<Int, Int> {
+        val elves: MutableList<MutableList<Int>> = mutableListOf()
+        var elf: MutableList<Int> = mutableListOf()
+        for (line in lines) {
+            if (line.isEmpty()) {
+                elves.add(elf)
+                elf = mutableListOf()
+                continue
+            }
+
+            elf.add(line.toInt())
+        }
+        elves.add(elf)
+
+        val calories: MutableList<Int> = mutableListOf()
+        for (e in elves) {
+            calories.add(e.sum())
+        }
+
+        val sortedCalories = calories.sortedBy { it }.asReversed()
+
+        val part1Answer = sortedCalories[0]
+
+        val part2Answer = sortedCalories.subList(0, 3).sum()
+
+        return Pair(part1Answer, part2Answer)
+    }
+}
---
+++ src/test/kotlin/Day1Test.kt
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+
+class Day1Test {
+
+    @Test
+    @DisplayName("Confirm it passes test data")
+    fun testData() {
+        Day1("src/test/resources/day1/test.txt").solve().let {
+            assertEquals(24000, it.first)
+            assertEquals(45000, it.second)
+        }
+    }
+
+    @Test
+    @DisplayName("Confirm it passes real data")
+    fun testReal(){
+        Day1("src/test/resources/day1/input.txt").solve().let {
+            println(it.first)
+            println(it.second)
+
+            assertTrue(true)
+        }
+    }
+}
