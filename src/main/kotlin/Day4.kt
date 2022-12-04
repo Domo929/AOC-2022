0d5e564d54987f53efd8967c74cef6174609b4f6
Dominic Cupo <Dominic.Cupo@gmail.com>
2022-12-04 14:35:57 -0500

    Cleanup
    Remove toInt
    
    move duplicate code to maps

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -3,25 +3,35 @@ class Day4(path: String) : Day(path) {
     override fun solve(): Pair<Int, Int> {
         val pairs: MutableList<Pair<Pair<Int, Int>, Pair<Int, Int>>> = mutableListOf()
         for (line in lines) {
-            val (e1, e2) = line.split(",")
-
-            val (e1sl, e1sh) = e1.split("-")
-            val e1p = Pair(e1sl.toInt(), e1sh.toInt())
-
-            val (e2sl, e2sh) = e2.split("-")
-            val e2p = Pair(e2sl.toInt(), e2sh.toInt())
-
-            pairs.add(Pair(e1p, e2p))
+            pairs.add(split(line))
         }
 
         return Pair(solvePart(pairs, isTotalOverlap), solvePart(pairs, isSemiOverlap))
+    }
+
+    private fun split(line: String): Pair<Pair<Int, Int>, Pair<Int, Int>> {
+        val (e1, e2) = line.split(",")
+
+        val (e1sl, e1sh) = e1.split("-").map { it.toInt() }
+        val e1p = Pair(e1sl, e1sh)
+
+        val (e2sl, e2sh) = e2.split("-").map { it.toInt() }
+        val e2p = Pair(e2sl, e2sh)
+
+        return Pair(e1p, e2p)
     }
 
     private fun solvePart(
         elfPairs: MutableList<Pair<Pair<Int, Int>, Pair<Int, Int>>>,
         f: (Pair<Int, Int>, Pair<Int, Int>) -> Boolean
     ): Int {
-        return elfPairs.map { f(it.first, it.second) }.map { toInt(it) }.reduce { sum, element -> sum + element }
+        return elfPairs.map { f(it.first, it.second) }.map {
+            if (it) {
+                1
+            } else {
+                0
+            }
+        }.reduce { sum, element -> sum + element }
     }
 
 
@@ -32,13 +42,6 @@ class Day4(path: String) : Day(path) {
 
     private val isSemiOverlap: (Pair<Int, Int>, Pair<Int, Int>) -> Boolean =
         { e1: Pair<Int, Int>, e2: Pair<Int, Int> ->
-               e1.second >= e2.first && e1.first <= e2.second
+            e1.second >= e2.first && e1.first <= e2.second
         }
-
-    private fun toInt(b: Boolean): Int {
-        if (b) {
-            return 1
-        }
-        return 0
-    }
 }
