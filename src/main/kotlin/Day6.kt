bb18251bb633160289b70974dd57787055d93c0d
Dominic Cupo <Dominic.Cupo@gmail.com>
2022-12-06 10:02:36 -0500

    Day6

---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,20 @@
+class Day6(path: String): Day(path) {
+
+    override fun solve(): Pair<Any, Any> {
+        if(lines.size !=1) {
+            throw Exception("Invalid input")
+        }
+
+        return Pair(findNDistinct(lines[0], 4), findNDistinct(lines[0], 14))
+    }
+
+    private fun findNDistinct(line: String, n: Int): Int {
+        for(i in n until line.toList().size){
+            if (line.substring(i-n, i).toSet().size == n) {
+                return i
+            }
+        }
+
+        return -1
+    }
+}
---
+++ src/test/kotlin/Day6Test.kt
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.DisplayName
+
+class Day6Test {
+
+    @Test
+    @DisplayName("Confirm it passes test data")
+    fun testData() {
+        Day6("src/test/resources/day6/test.txt").solve().let {
+            assertEquals(7, it.first)
+            assertEquals(19, it.second)
+        }
+    }
+
+    @Test
+    fun testReal(){
+        Day6("src/test/resources/day6/input.txt").solve().let {
+            println(it.first)
+            println(it.second)
+
+            assertTrue(true)
+        }
+    }
+}
